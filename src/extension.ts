583f4d351b7ffb1d921fcb331980b7650de03619
Myles Murphy <mylesmurphy@Myless-MacBook-Pro.local>
2023-12-23 23:34:34 -0500

    final working draft of extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,44 +1,87 @@
 import * as vscode from 'vscode';
-import { Project, TypeAliasDeclaration, MappedTypeNode } from 'ts-morph';
+import { Project, TypeAliasDeclaration } from 'ts-morph';
 import { ulid } from 'ulid';
+import { format } from 'prettier';
+
+const EXTENSION_NAME = 'prettify-ts';
 
 export function activate(context: vscode.ExtensionContext) {
+  // Get the current configuration
+  const config = vscode.workspace.getConfiguration(EXTENSION_NAME);
+  let enableHover = config.get('enableHover', true);
+
+  // Register the toggleHover command
+  context.subscriptions.push(
+    vscode.commands.registerCommand(`${EXTENSION_NAME}.toggleHover`, async () => {
+      enableHover = !enableHover;
+      await config.update('enableHover', enableHover, vscode.ConfigurationTarget.Global);
+    })
+  );
+
+  // Check if enableHover is changed
+  context.subscriptions.push(
+    vscode.workspace.onDidChangeConfiguration((e) => {
+      if (e.affectsConfiguration(`${EXTENSION_NAME}.enableHover`)) {
+        enableHover = config.get('enableHover', true);
+      }
+    })
+  );
+
+  // Register the hover provider
   context.subscriptions.push(
     vscode.languages.registerHoverProvider('typescript', {
-      provideHover(document, position, token) {
-        // Create a new Project instance
+      async provideHover(document, position, token) {
+        // Return early if the hover is disabled
+        if (!enableHover) {
+          return;
+        }
+
+        // Create a new Project and type checker instance
         const project = new Project();
+        const typeChecker = project.getTypeChecker();
+
+        // Add the file to the project
         const sourceFile = project.addSourceFileAtPath(document.fileName);
+
+        // Use the current document's text as the source file's text
+        const content = document.getText();
+        sourceFile.replaceWithText(content);
+
+        // Get the offset of the position
         const offset = document.offsetAt(position);
       
         // Get the node at the offset
-        let node = sourceFile.getDescendantAtPos(offset);
+        const node = sourceFile.getDescendantAtPos(offset);
         if (!node) {
           return;
         }
         
         // Get the type of the node
-        const typeChecker = project.getTypeChecker();
         const type = typeChecker.getTypeAtLocation(node);
-      
-        // Get the name of the type
-        let typeName = type.getText();
+        const typeFullName = type.getText();
         const typeSymbol = type.getSymbol();
+
+        if (!typeSymbol) {
+          return;
+        }
+
+        // Get the name of the type
+        let typeName = typeSymbol.getEscapedName();
+
         // Get what kind of type the type is, ex. 'class', 'interface', 'type alias', etc.
         const declaration = typeSymbol?.getDeclarations()[0];
         const typeKind = declaration?.getKindName();
 
-        // Get the parent of the identifier if the type is a type alias
-        if (typeKind !== 'InterfaceDeclaration' && typeKind !== 'ClassDeclaration') {
-          node = node.getParent();
-          // Check if the parent node is a type alias
-          if (node && TypeAliasDeclaration.isTypeAliasDeclaration(node)) {
-            typeName = node.getName();
-          }
+        const parentNode = node.getParent();
+        const parentNodeText = parentNode?.getText();
+
+        // Check if the parent node is a type alias
+        if (parentNode && TypeAliasDeclaration.isTypeAliasDeclaration(parentNode)) {
+          typeName = parentNode.getName();
         }
 
         // Don't show the hover if the type is any or if the type kind is undefined
-        if (typeName === 'any' || !typeKind) {
+        if (typeName === 'any' || !typeKind || !typeName) {
           return;
         }
       
@@ -50,25 +93,30 @@ export function activate(context: vscode.ExtensionContext) {
           name: `Prettify_${prettifyId}`,
           typeParameters: [{ name: 'T' }],
           isExported: false,
-          type: `T extends String | Number | Boolean | Date | RegExp | Function | Symbol | undefined | null | void
+          type: `T extends String | Number | Boolean | Date | RegExp | Function | Symbol
             ? T
             : T extends Array<infer U>
-            ? Prettify<U>[]
+            ? Prettify_${prettifyId}<U>[]
             : T extends object
-            ? { [P in keyof T]: Prettify<T[P]> } & unknown
+            ? { [P in keyof T]: Prettify_${prettifyId}<T[P]> } & unknown
             : T;`,
         });
-            
+
         // Add a new type alias that uses Prettify with the type of the node as the generic parameter
         sourceFile.addTypeAlias({
           name: `PrettifiedType_${prettifyId}`,
           isExported: false,
-          type: `Prettify_${prettifyId}<${typeName}>`,
+          type: `Prettify_${prettifyId}<${typeFullName}>`,
         });
       
-        // Get the type of the PrettifiedType type alias and get the text of the type node
+        // Get the type of the PrettifiedType type alias and get the text of the new type node
         const prettifiedType = typeChecker.getTypeAtLocation(sourceFile.getTypeAliasOrThrow(`PrettifiedType_${prettifyId}`).getTypeNodeOrThrow());
-        const prettifiedTypeString = prettifiedType.getText().replace(/; /g, ';\n'); // Add linebreaks which will get formatted later
+        const prettifiedTypeString = prettifiedType.getText().replace(/\s+/g, '\n'); // Replace all whitespace with newlines which will be used for formatting
+
+        // If the prettified type isn't an object, then return early
+        if (prettifiedTypeString[0] !== '{') {
+          return;
+        }
 
         // Format the declaration string based on the kind of type
         let declarationString;
@@ -86,15 +134,16 @@ export function activate(context: vscode.ExtensionContext) {
             break;
         }
         
-        // Format the type string using the TypeScript formatter in a temporary file
-        const formatFile = project.createSourceFile('temp.ts', declarationString);
-        formatFile.formatText();
-        const formattedTypeString = formatFile.getFullText();
-        
+        const formattedTypeString = await format(declarationString, { parser: 'typescript' });
+
+        // Check formattedTypeString with parentNodeText
+        // if they're the same with the exception of whitespace and semicolons, then return early
+        if (formattedTypeString.replace(/\s+/g, '').replace(/;/g, '') === parentNodeText?.replace(/\s+/g, '').replace(/;/g, '')) {
+          return;
+        }
+
         // Format the hover text with Markdown
         const hoverText = new vscode.MarkdownString();
-        // Add a title
-        hoverText.appendMarkdown(`**${typeName}**`);
         hoverText.appendCodeblock(formattedTypeString, 'typescript');
         
         return new vscode.Hover(hoverText);
