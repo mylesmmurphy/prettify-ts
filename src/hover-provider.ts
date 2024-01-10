e03c9ddba3f4170b757f9deb4e3b8db4c8972e53
redhair <tombonanni@gmail.com>
2024-01-10 17:33:46 -0500

    feat: tsx support (#11)
    * register hoverprovider for typescriptreact, add associated project initializer
    
    * lint
    
    * Add Tom Bonanni to contributors list
    
    * bump minor version
    
    * chore: version update
    
    ---------
    
    Co-authored-by: Myles Murphy <mylesmurphy@Myless-MacBook-Pro.local>

--- src/hover-provider.ts
+++ src/hover-provider.ts
@@ -6,41 +6,50 @@ import { getProject } from './project-cache'
 import { washString } from './helpers'
 
 export function registerHoverProvider (context: vscode.ExtensionContext): void {
-  context.subscriptions.push(
-    vscode.languages.registerHoverProvider('typescript', {
-      async provideHover (document, position, token) {
-        const config = vscode.workspace.getConfiguration(EXTENSION_ID)
-        const enableHover = config.get('enableHover', true)
+  async function provideHover (
+    document: vscode.TextDocument,
+    position: vscode.Position
+  ): Promise<vscode.Hover | undefined> {
+    const config = vscode.workspace.getConfiguration(EXTENSION_ID)
+    const enableHover = config.get('enableHover', true)
 
-        if (!enableHover) return
+    if (!enableHover) {
+      await Promise.resolve(undefined)
+      return
+    }
 
-        const content = document.getText()
-        const offset = document.offsetAt(position)
-        const fileName = document.fileName
+    const content = document.getText()
+    const offset = document.offsetAt(position)
+    const fileName = document.fileName
 
-        let typeString = await prettifyType(fileName, content, offset)
-        if (typeString === undefined) return
+    return await prettifyType(fileName, content, offset).then((typeString) => {
+      if (typeString === undefined) return undefined
 
-        // Ignore hover if the type is already displayed from TS quick info
-        if (typeString.startsWith('type') || typeString.startsWith('const')) {
-          const project = getProject(fileName)
-          const languageService = project.getLanguageService().compilerObject
-          const quickInfo = languageService.getQuickInfoAtPosition(fileName, offset)
-          const quickInfoText = ts.displayPartsToString(quickInfo?.displayParts)
+      // Ignore hover if the type is already displayed from TS quick info
+      if (typeString.startsWith('type') || typeString.startsWith('const')) {
+        const project = getProject(fileName)
+        const languageService = project.getLanguageService().compilerObject
+        const quickInfo = languageService.getQuickInfoAtPosition(
+          fileName,
+          offset
+        )
+        const quickInfoText = ts.displayPartsToString(quickInfo?.displayParts)
 
-          if (washString(quickInfoText).includes(washString(typeString))) {
-            return
-          }
+        if (washString(quickInfoText).includes(washString(typeString))) {
+          return undefined
         }
-
-        if (typeString.length > MARKDOWN_MAX_LENGTH) {
-          typeString = typeString.substring(0, MARKDOWN_MAX_LENGTH) + '...'
-        }
-
-        const hoverText = new vscode.MarkdownString()
-        hoverText.appendCodeblock(typeString, 'typescript')
-        return new vscode.Hover(hoverText)
       }
+
+      if (typeString.length > MARKDOWN_MAX_LENGTH) {
+        typeString = typeString.substring(0, MARKDOWN_MAX_LENGTH) + '...'
+      }
+
+      const hoverText = new vscode.MarkdownString()
+      hoverText.appendCodeblock(typeString, document.languageId)
+      return new vscode.Hover(hoverText)
     })
-  )
+  }
+
+  context.subscriptions.push(vscode.languages.registerHoverProvider('typescript', { provideHover }))
+  context.subscriptions.push(vscode.languages.registerHoverProvider('typescriptreact', { provideHover }))
 }
