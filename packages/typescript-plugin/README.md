657571e8596e1fb5ec7e5c9791b763819d034585
Myles Murphy <mylesmurphy@Myless-MacBook-Pro.local>
2024-05-12 13:44:42 -0400

    initial setup

---
+++ packages/typescript-plugin/README.md
@@ -0,0 +1,3 @@
+# TypeScript Language Server Plugin
+
+This is a TypeScript Language Server Plugin that enhances the functionality of the TypeScript Language Server.
---
+++ packages/typescript-plugin/src/index.ts
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import type * as ts from 'typescript/lib/tsserverlibrary'
+
+function init (modules: { typescript: typeof ts }): ts.server.PluginModule {
+  const ts = modules.typescript
+
+  function create (info: ts.server.PluginCreateInfo): ts.LanguageService {
+    // Set up decorator object
+    const proxy: ts.LanguageService = Object.create(null)
+    for (const k of Object.keys(info.languageService) as Array<keyof ts.LanguageService>) {
+      const x = info.languageService[k]!
+      // @ts-expect-error - JS runtime trickery which is tricky to type tersely
+      proxy[k] = (...args: unknown[]) => x.apply(info.languageService, args)
+    }
+
+    // Remove specified entries from completion list
+    proxy.getCompletionsAtPosition = (fileName, position, options) => {
+      const prior = info.languageService.getCompletionsAtPosition(fileName, position, options)
+      if (!prior) return
+
+      const oldLength = prior.entries.length
+      const whatToRemove = ['foo', 'bar']
+      prior.entries = prior.entries.filter(e => !whatToRemove.includes(e.name))
+
+      // Sample logging for diagnostic purposes
+      if (oldLength !== prior.entries.length) {
+        const entriesRemoved = oldLength - prior.entries.length
+        info.project.projectService.logger.info(
+            `Removed ${entriesRemoved} entries from the completion list`
+        )
+      }
+
+      return prior
+    }
+
+    return proxy
+  }
+
+  return { create }
+}
+
+export = init
