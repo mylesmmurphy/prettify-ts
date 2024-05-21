991f22aa8feaab6462b9db1cd879018b9232ce94
Myles Murphy <mylesmurphy@Myless-MacBook-Pro.local>
2024-05-21 16:59:54 -0400

    fixed tsc

--- packages/vscode-extension/src/extension.ts
+++ packages/vscode-extension/src/extension.ts
@@ -1,13 +1,6 @@
-import * as vscode from 'vscode'
+import type * as vscode from 'vscode'
 import { registerHoverProvider } from './hover-provider'
 
-const outputChannel = vscode.window.createOutputChannel('Prettify-TS')
-
 export function activate (context: vscode.ExtensionContext): void {
-  outputChannel.show()
-  registerHoverProvider(context, outputChannel)
-}
-
-export function deactivate (): void {
-  outputChannel.dispose()
+  registerHoverProvider(context)
 }
