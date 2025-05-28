17acd10e1f4a912a408a0e3229f215881b93e170
Myles Murphy <murphymmyles@gmail.com>
2025-05-28 11:43:28 -0400

    npm

---
+++ scripts/post-build.js
@@ -0,0 +1,13 @@
+/**
+ * Post Build Script
+ * Copies the built TS Plugin to the Extension node_modules directory
+ */
+const fs = require('fs-extra');
+const path = require('path');
+
+const pluginDir = path.resolve(__dirname, '../packages/typescript-plugin');
+const extensionNodeModules = path.resolve(__dirname, '../packages/vscode-extension/node_modules/@prettify-ts/typescript-plugin');
+
+fs.ensureDirSync(extensionNodeModules);
+fs.copySync(path.join(pluginDir, 'package.json'), path.join(extensionNodeModules, 'package.json'));
+fs.copySync(path.join(pluginDir, 'out'), path.join(extensionNodeModules, 'out'));
