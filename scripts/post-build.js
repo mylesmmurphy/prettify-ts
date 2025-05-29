30950d9392e5787919f050d7afc7080463a61836
Myles Murphy <murphymmyles@gmail.com>
2025-05-28 21:52:24 -0400

    fix

--- scripts/post-build.js
+++ scripts/post-build.js
@@ -6,7 +6,10 @@ const fs = require('fs-extra');
 const path = require('node:path');
 
 const pluginDir = path.resolve(__dirname, '../packages/typescript-plugin');
-const extensionNodeModules = path.resolve(__dirname, '../packages/vscode-extension/node_modules/@prettify-ts/typescript-plugin');
+const extensionNodeModules = path.resolve(__dirname, '../packages/vscode-extension/node_modules/@prettify-ts/typescript-plugin-dist');
+
+// Clean the target directory first
+fs.removeSync(extensionNodeModules);
 
 fs.ensureDirSync(extensionNodeModules);
 fs.copySync(path.join(pluginDir, 'package.json'), path.join(extensionNodeModules, 'package.json'));
