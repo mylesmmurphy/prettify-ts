582f2229cd01e4e1f60eb5cf7485ba14b1bf13e0
Myles Murphy <murphymmyles@gmail.com>
2025-06-03 11:35:22 -0400

    update todos, update readmes

--- test/suite/commands.test.ts
+++ test/suite/commands.test.ts
@@ -1,1 +1,2 @@
 // TODO
+// Test all commands Copy prettified type, Copy fully prettified type, toggle hover
--- test/suite/settings.test.ts
+++ test/suite/settings.test.ts
@@ -1,1 +1,2 @@
 // TODO
+// Test all settings: Type Indentation, Max Depth, Max Properties, Max Sub-Properties, Max Union Members, Max Function Signatures, Unwrap Functions, Unwrap Arrays, Unwrap Generic Arguments Type Names, Hide Private Properties**: If enabled, hides private properties and methods., Skipped Type Names, Max Characters
